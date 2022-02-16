66130fa0a75e5f99d42b61875b83ed6e1923f6b7
Caio Fleury <caio.fleury.r@gmail.com>
2022-02-16 16:46:38 -0300

    Removed superfluous routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -19,18 +19,6 @@ router.get('/', (req, res) => {
 });
 
 router.get('/imu', (req, res) => {
-  res.status(404).json({
-    message: `Error: For the html page please query 'host:port/imu'\nFor the file's top n lines please query 'host:port/api/imu/:n'`
-  })
-})
-
-router.get('/gps', (req, res) => {
-  res.status(404).json({
-    message: `Error: For the html page please query 'host:port/gps'\nFor the file's top n lines please query 'host:port/api/gps/:n'`
-  })
-})
-
-router.get('/imu', (req, res) => {
   ParserIMU.readLines((data) => {
     res.json({
       message: data
